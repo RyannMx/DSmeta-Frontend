dc7770c5efd68543dd207796b7973376e6385236
ryannmx <ryanshift22@gmail.com>
2022-07-15 20:54:53 -0300

    Toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
 import Salescard from "./components/Salescard"
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
 
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
--- src/components/NotificationButton/index.tsx
+++ src/components/NotificationButton/index.tsx
@@ -2,6 +2,7 @@ import './style.css';
 import icon from '../../assets/Vector.svg';
 import axios from "axios";
 import { BASE_URL } from '../../utils/request';
+import { toast } from 'react-toastify';
 
 type prpos = {
   saleid: number
@@ -11,6 +12,7 @@ function handleClick(id: number) {
   axios(`${BASE_URL}/sales/${id}/notification`)
     .then(response => {
       console.log("sucesso");
+      toast.info("SMS foi enviado com sucesso!");
     })
 }
 
